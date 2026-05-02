967c56bb48515011a0227e323a325a39104e14d1
Abeloider <abeljiang@AbelJiang.>
2026-05-02 18:57:38 +0200

    este codigo funciona como antes todo igual ejercicio 1

--- ejercicios/Ejercicio1.java
+++ ejercicios/Ejercicio1.java
@@ -22,8 +22,6 @@ public class Ejercicio1 {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
-			s.start();
 			try {
 				s.join();
 			} catch (InterruptedException e) {
@@ -49,6 +47,61 @@ class Cuadrado extends Thread {
 		this.matriz[2][2] = a9;
 		Ejercicio1.lock.lock();
 		try {
+			System.out.println("A x A");
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					System.out.print(matriz[i][j] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+			System.out.println("x\n");
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					System.out.print(matriz[i][j] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+			System.out.println("2A");
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					//Calculo de la sumaclas
+					matriz[i][j] = matriz[i][j] * 2;
+					//Calculo de la suma
+					matriz[i][j] = matriz[i][j] + matriz[i][j];
+					System.out.print(matriz[i][j] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		} finally {
+			Ejercicio1.lock.unlock();
+		}
+	}
+}
+
+
+
+class Suma extends Thread {
+	private int matriz[][];
+	//Creacion de la matriz a partir de los numeros
+	public Suma(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) {
+		this.matriz = new int[3][3];
+		this.matriz[0][0] = a1;
+		this.matriz[0][1] = a2;
+		this.matriz[0][2] = a3;
+		this.matriz[1][0] = a4;
+		this.matriz[1][1] = a5;
+		this.matriz[1][2] = a6;
+		this.matriz[2][0] = a7;
+		this.matriz[2][1] = a8;
+		this.matriz[2][2] = a9;
+	}
+
+	public void run() {
+		Ejercicio1.lock.lock();
+		try {
 			System.out.println("A + A");
 			for (int i = 0; i < 3; i++) {
 				for (int j = 0; j < 3; j++) {
@@ -70,8 +123,6 @@ class Cuadrado extends Thread {
 			System.out.println("2A");
 			for (int i = 0; i < 3; i++) {
 				for (int j = 0; j < 3; j++) {
-					//Calculo de la sumaclas
-					matriz[i][j] = matriz[i][j] * 2;
 					//Calculo de la suma
 					matriz[i][j] = matriz[i][j] + matriz[i][j];
 					System.out.print(matriz[i][j] + " ");
